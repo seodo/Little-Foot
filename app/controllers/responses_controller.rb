fbbf65d50f8672b1918da8840f8ff566b5d825be
Julia Giraldi <email": null>
2016-05-13 19:57:21 -0400

    adds functionality to create response objects

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,11 +1,15 @@
 class ResponsesController < ApplicationController
 
 def create
-  binding.pry
   @response = Response.new(question_id: params[:question_id].to_i, survey_id: params[:survey_id].to_i)
-
-  # iterate through the hash, for each key value pair instantiate a new response
-  # iterate through each key value pair, if key == impact.item.name, && value is greater than 0, then create a new response with that impact item and value.
+@responses = []
+  params.each do |k,v|
+    if ImpactItem.find_by(name: k) && v.to_i > 0
+      impact_item = ImpactItem.find_by(name: k)
+     @responses << Response.create(question_id: params[:question_id].to_i, survey_id: params[:survey_id].to_i, impact_item_id: impact_item.id, quantity: v.to_i)
+    end
+  end
+  # logic to get to the next question (2)
 end
 
 
--- db/migrate/20160512232114_create_responses.rb
+++ db/migrate/20160512232114_create_responses.rb
@@ -2,7 +2,7 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
       t.integer :impact_item_id
-      t.decimal :quantity
+      t.integer :quantity
       t.integer :question_id, null: false
       t.integer :survey_id, null: false
 
