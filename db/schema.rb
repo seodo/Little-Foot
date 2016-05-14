ba544df7e836a2000cd573591bdc50a5a768402c
Patrick Skelley <email": null>
2016-05-14 15:27:49 -0400

    created transportation choices and question

--- db/schema.rb
+++ db/schema.rb
@@ -30,12 +30,13 @@ ActiveRecord::Schema.define(version: 20160513210848) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "content",      null: false
-    t.integer  "category_id",  null: false
+    t.string   "content",                null: false
+    t.integer  "category_id",            null: false
     t.integer  "follow_up_id"
-    t.boolean  "choices"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.boolean  "food_choices"
+    t.boolean  "transportation_choices"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "responses", force: :cascade do |t|
