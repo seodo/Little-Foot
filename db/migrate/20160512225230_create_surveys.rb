ab02c351f3522b7bb20d884c0a89b5ecab8a9b82
Patrick Skelley <email": null>
2016-05-16 20:47:41 -0400

    adds google map on survey show, adds pin to map where survey was taken

--- db/migrate/20160512225230_create_surveys.rb
+++ db/migrate/20160512225230_create_surveys.rb
@@ -2,6 +2,8 @@ class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
       t.integer :user_id
+      t.float :latitude
+      t.float :longitude
 
       t.timestamps null: false
     end
