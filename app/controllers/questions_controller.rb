35d2162c43514a552af78a56d0d3059fed9a4f8f
john seo <email": null>
2016-05-18 11:21:21 -0400

    pass facts array to question controller and add fact divs in question view file

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,10 @@ def show
 
   @question = Question.find_by(id: params[:id])
   @survey = Survey.find_by(id: params[:survey_id])
+  @foodfacts = Fact.select {|fact| fact.category_id == 1}
+  @transfacts = Fact.select {|fact| fact.category_id == 2}
+  @lifefacts = Fact.select {|fact| fact.category_id == 3}
+
 end
 
 
